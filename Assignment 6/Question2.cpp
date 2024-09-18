55b997860ce63a128d043263f8ec6ba74cffa791
Harsh Dixit <harshdixitt10@gmail.com>
2024-09-18 18:34:47 +0530

    Assign 6 Ques 2 Updated..

--- Assignment 6/Question2.cpp
+++ Assignment 6/Question2.cpp
@@ -4,234 +4,188 @@
 	KD1-86691-Harsh
 */
 
-
 #include <iostream>
-#include <typeinfo>
-
 using namespace std;
-class Person
-{
-    string name;
 
-public:
-    Person()
-    {
-        cout << "Person()" << endl;
-    }
-    Person(string name)
-    {
-        cout << "Person(string)" << endl;
-        this->name = name;
-    }
-    virtual void accept()
-    {
-        cout << "Enter the name - ";
-        cin >> name;
-    }
-    virtual void display()
-    {
-        cout << "Name - " << name << endl;
-    }
-};
+class Employee{
 
-class Employee : public Person
-{
-    int empid;
+    int id;
     double salary;
 
 public:
-    Employee()
-    {
-        cout << "Employee()" << endl;
-    }
-    Employee(int empid, string name, double salary) : Person(name)
-    {
-        cout << "Employee(int,string,double)" << endl;
-        this->empid = empid;
-        this->salary = salary;
-        // this->name = name; // can perform  if data member is protected
-    }
-    int getEmpid()
-    {
-        return empid;
-    }
-    void accept()
-    {
-        cout << "Enter empid - ";
-        cin >> empid;
-        Person::accept();
-        cout << "Enter Salary - ";
-        cin >> salary;
+
+    Employee(){
+        id = 0;
+        salary = 0;
+    }    
+
+    Employee(int id, double salary){
+        this-> id = id;
+        this-> salary = salary;
     }
 
-    void display()
-    {
-        cout << "Empid -  " << empid << endl;
-        Person::display();
-        cout << "Salary -  " << salary << endl;
+    void setId(int id){
+        this->id = id;
+    }
+
+     void setSalary(double salary){
+        this->salary = salary;
+    }
+
+    int getId(){
+        return id;
+    }
+
+    double getSalary(){
+        return salary;
+    }
+
+    void accept(){
+        cout<<"Enter Id : "<<endl;
+        cin>>id;
+        cout<<"Enter Salary : "<<endl;
+        cin>>salary;
+    }
+
+    void display(){
+        cout<<"Id : "<<id<<endl;
+        cout<<"Salary : "<<salary<<endl;
     }
 };
 
-class Student : public Person
-{
-    int rollno;
-    double marks;
+class Manager : virtual public Employee{
+
+    double bonus;
+
+protected:
+
+    void acceptManager(){
+        cout<<"Enter bonus : "<<endl;
+        cin>>bonus;
+    }    
+
+    void displayManager(){
+        cout<<"Bonus is : "<<bonus<<endl;
+    }
 
 public:
-    Student()
-    {
+
+    Manager(){
+        bonus = 0;
+    }    
+
+    Manager(int id, double salary, double bonus): Employee(id,salary)
+	{
+       this->bonus = bonus;
     }
-    Student(int rollno, string name, double marks) : Person(name)
-    {
-        this->rollno = rollno;
-        this->marks = marks;
+
+    void setBonus(double bonus)
+	{
+        this->bonus = bonus;
     }
+
+    double getBonus(){
+        return bonus;
+    }
+
     void accept()
-    {
-        cout << "Enter rollno - ";
-        cin >> rollno;
-        Person::accept();
-        cout << "Enter marks - ";
-        cin >> marks;
+	{
+        Employee::accept();
+        acceptManager();
     }
+
     void display()
-    {
-        cout << "Rollno - " << rollno << endl;
-        Person::display();
-        cout << "Marks - " << marks << endl;
+	{
+        Employee::display();
+        displayManager();
     }
-    int getrollno()
-    {
-        return this->rollno;
-    }
+
 };
 
-int findEmployee(Person **ptr, int index)
-{
-    int id;
-    cout << "Enter the empid to search the employee - ";
-    cin >> id;
+class Salesman : virtual public Employee{
 
-    for (int i = 0; i < index; i++)
-    {
-        if (typeid(*ptr[i]) == typeid(Employee))
-        {
-            Employee *eptr = (Employee *)ptr[i];
-            if (id == eptr->getEmpid())
-                return i;
-        }
+    double commision;
+
+protected:
+
+    void acceptSalesman(){
+        cout<<"Enter Commision : "<<endl;
+        cin>>commision;
+    }    
+
+    void displaySalesman(){
+        cout<<"Commision is : "<<commision<<endl;
     }
-    return -1;
-}
-int findStudent(Person **ptr, int index)
-{
-    int rno;
-    cout << "Enter the rollno to search the Student - ";
-    cin >> rno;
 
-    for (int i = 0; i < index; i++)
-    {
-        if (typeid(*ptr[i]) == typeid(Student))
-        {
-            Student *sptr = (Student *)ptr[i];
-            if (rno == sptr->getrollno())
-                return i;
-        }
+public:
+
+    Salesman(){
+        commision = 0;
+    }    
+
+    Salesman(int id, double salary, double commision): Employee(id,salary)
+	{
+       this->commision=commision;
     }
-    return -1;
-}
 
-enum EMenu
-{
-    EXIT,
-    ADD_EMPLOYEE,
-    ADD_STUDENT,
-    DISPLAY_EMPLOYEE,
-    DISPLAY_STUDENT,
-    FIND_EMPLOYEE,
-    FIND_STUDENT
+    void setCommission(double commision)
+	{
+        this->commision=commision;
+    }
+
+    double getCommision()
+	{
+        return commision;
+    }
+
+    void accept()
+	{
+        Employee::accept();
+        acceptSalesman();
+    }
+
+    void display()
+	{
+        Employee::display();
+        displaySalesman();
+    }
+
 };
 
-int menu()
-{
-    int choice;
-    cout << "*******" << endl;
-    cout << "0. EXIT" << endl;
-    cout << "1. Add Employee" << endl;
-    cout << "2. Add Student" << endl;
-    cout << "3. Display Employee" << endl;
-    cout << "4. Display Student" << endl;
-    cout << "5. Find Employee" << endl;
-    cout << "6. Find Student" << endl;
-    cout << "Enter your choice - ";
-    cin >> choice;
-    cout << "*******" << endl;
+class SalesManager : public Manager, public Salesman{
 
-    return choice;
-}
-
-int main()
-{
-    Person *arr[5];
-    int index = 0;
-    int choice;
-    while ((choice = menu()) != EXIT)
-    {
-        switch (EMenu(choice))
-        {
-        case ADD_EMPLOYEE:
-            if (index < 5)
-            {
-                arr[index] = new Employee;
-                arr[index]->accept();
-                index++;
-            }
-            else
-                cout << "Array is Full..." << endl;
-            break;
-        case ADD_STUDENT:
-        {
-            arr[index] = new Student;
-            arr[index]->accept();
-            index++;
-        }
-        break;
-        case DISPLAY_EMPLOYEE:
-            for (int i = 0; i < index; i++)
-                if (typeid(*arr[i]) == typeid(Employee))
-                    arr[i]->display();
-            break;
-        case DISPLAY_STUDENT:
-        {
-            for (int i = 0; i < index; i++)
-                if (typeid(*arr[i]) == typeid(Student))
-                    arr[i]->display();
-            break;
-        }
-        break;
-        case FIND_EMPLOYEE:
-        {
-            int i = findEmployee(arr, index);
-            if (i != -1)
-                arr[i]->display();
-            else
-                cout << "Employee not found..." << endl;
-        }
-        break;
-        case FIND_STUDENT:
-        {
-            int i = findStudent(arr, index);
-            if (i != -1)
-                arr[i]->display();
-            else
-                cout << "Student not found..." << endl;
-        }
-        break;
-        default:
-            cout << "Wrong choice ... " << endl;
-            break;
-        }
+public:
+    SalesManager()
+	{
     }
 
-    return 0;
+    SalesManager(int id, double sal, double bonus, double commision):Employee(id,sal)
+	{
+        setBonus(bonus);
+        setCommission(commision);
+    }
+
+    void accept(){
+        Employee::accept();
+        acceptManager();
+        acceptSalesman();
+    }
+
+    void display(){
+        Employee::display();
+        displayManager();
+        displaySalesman();
+    }
+
+};
+
+
+int main(){
+    SalesManager s1;
+    s1.accept();
+    s1.display();
+
+    SalesManager s2(2,20000,5000,6000);
+    s2.display();
+	return 0;
 }
